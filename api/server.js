6916f820bcab836a656826da9fddebd3652e3809
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-09 14:02:05 -0600

    Implement api server and update index to match

--- api/server.js
+++ api/server.js
@@ -1,2 +1,19 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+const postsRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('api/posts', postsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Blog Posts API</h2>
+    <p>This is a simple API for blog posts built for assignment 4.1.2</p>
+  `);
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // require your server and launch it here
-const express = require('express');
-
-const server = express();
+const server = require('./api/server');
 
 const port = 8000;
 
