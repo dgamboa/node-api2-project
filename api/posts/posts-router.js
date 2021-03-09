5b53847d47e92ef67442458f8830788a3f06af2c
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-09 14:29:57 -0600

    Implement get by id endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -10,8 +10,27 @@ router.get('/', async (req, res) => {
     const posts = await Posts.find();
     res.status(200).json(posts);
   } catch(err) {
-    res.status(500).json({ message: "Error retrieving posts" });
+    res.status(500).json({ message: "The posts information could not be retrieved" });
   }
 });
+
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id)
+    post
+      ? res.status(200).json(post)
+      : res.status(404).json({ message: "The post with the specified ID does not exist" })
+  } catch (err) {
+    res.status(500).json({ message: "The posts information could not be retrieved" });
+  }
+});
+
+// router.get('/:id', async (req, res) => {
+//   try {
+
+//   } catch (err) {
+//     res.status(500).json({ message: "The posts information could not be retrieved" });
+//   }
+// });
 
 module.exports = router;
