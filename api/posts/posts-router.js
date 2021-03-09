d61e4415f0efd74932fc22573702bca023f9d59e
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-09 14:17:51 -0600

    Implement get endpoint for posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const express = require('express');
+
+const router = express.Router();
+
+const Posts = require('./posts-model');
+
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    res.status(200).json(posts);
+  } catch(err) {
+    res.status(500).json({ message: "Error retrieving posts" });
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const server = express();
 
 server.use(express.json());
 
-server.use('api/posts', postsRouter);
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
