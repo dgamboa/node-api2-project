d73afbd0f0e7e4d9422a364b3d8c4e4ac20d6225
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-09 13:57:08 -0600

    Set up index.js to create express server and listen on port 8000

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const express = require('express');
+
+const server = express();
+
+const port = 8000;
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
