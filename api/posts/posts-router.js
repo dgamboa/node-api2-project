bf0b2d57257182d80f77358aa87a3f6bf3b9c0f9
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-09 15:40:46 -0600

    Return deleted object for delete endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -63,10 +63,13 @@ router.delete('/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
-    const postToDelete = await Posts.remove(id);
-    postToDelete
-      ? res.status(200).json(postToDelete)
-      : res.status(404).json({ message: "The post with the specified ID does not exist"})
+    const postToDelete = await Posts.findById(id);
+    if (postToDelete) {
+      const deletedPost = await Posts.remove(id);
+      res.status(200).json(postToDelete)
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist"})
+    }
   } catch (err) {
     res.status(500).json({ message: "The post could not be removed" });
   }
